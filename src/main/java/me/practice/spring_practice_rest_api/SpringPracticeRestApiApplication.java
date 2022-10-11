8393e3050b8e9623bab16af1ea6e284ca192083c
2sssg <lsg020300@naver.com>
2022-10-11 22:54:44 +0900

    first commit

---
+++ src/main/java/me/practice/spring_practice_rest_api/SpringPracticeRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.practice.spring_practice_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/practice/spring_practice_rest_api/SpringPracticeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.practice.spring_practice_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
