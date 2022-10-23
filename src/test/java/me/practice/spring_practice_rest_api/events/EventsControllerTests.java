cd09244e12fab5bcbfe20204f9dcd62cb4e79531
2sssg <lsg020300@naver.com>
2022-10-23 18:09:02 +0900

    feat : application.yaml and postgresql configuration

--- src/test/java/me/practice/spring_practice_rest_api/events/EventsControllerTests.java
+++ src/test/java/me/practice/spring_practice_rest_api/events/EventsControllerTests.java
@@ -33,6 +33,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -41,6 +42,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 class EventsControllerTests {
 
 	@Autowired MockMvc mockMvc;
