d61e1b1f0d0d7cd5944f955d26fd42ee627866f3
2sssg <lsg020300@naver.com>
2022-10-28 22:01:31 +0900

    feat: authentication and additional information

--- src/main/java/me/practice/spring_practice_rest_api/events/EventController.java
+++ src/main/java/me/practice/spring_practice_rest_api/events/EventController.java
@@ -7,6 +7,9 @@ import java.util.Optional;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.practice.spring_practice_rest_api.accounts.Account;
+import me.practice.spring_practice_rest_api.accounts.AccountAdapter;
+import me.practice.spring_practice_rest_api.accounts.CurrentUser;
 import me.practice.spring_practice_rest_api.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
@@ -15,7 +18,12 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,7 +46,9 @@ public class EventController {
 	private final EventValidator eventValidator;
 
 	@PostMapping
-	public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+	public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors,
+			@CurrentUser Account currentUser) {
+
 		if (errors.hasErrors())
 			return getErrorsResourceResponseEntity(errors);
 
@@ -49,6 +59,7 @@ public class EventController {
 
 		Event event = modelMapper.map(eventDto, Event.class);
 		event.update();
+		event.setManager(currentUser);
 		Event newEvent = this.eventRepository.save(event);
 		WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
 		URI createdUri = selfLinkBuilder.toUri();
@@ -60,34 +71,40 @@ public class EventController {
 	}
 
 	@GetMapping
-	public ResponseEntity queryEvent(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+	public ResponseEntity queryEvent(Pageable pageable, PagedResourcesAssembler<Event> assembler,
+			@CurrentUser Account currentUser) {
 
 		Page<Event> page = this.eventRepository.findAll(pageable);
-		var pagedResources =
-				assembler.toModel(page, EventResource::new);
+		var pagedResources = assembler.toModel(page, EventResource::new);
+		pagedResources.add(Link.of("/docs/index.html#resources-events-list","profile"));
 
-		pagedResources
-				.add(Link.of("/docs/index.html#resources-events-list","profile"));
+		if (currentUser != null)
+			pagedResources.add(linkTo(EventController.class).withRel("create-event"));
+
 		return ResponseEntity.ok(pagedResources);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity getEvent(@PathVariable Integer id) {
+	public ResponseEntity getEvent(@PathVariable Integer id, @CurrentUser Account currentUser) {
 
 		Optional<Event> optionalEvent = this.eventRepository.findById(id);
 
 		if (optionalEvent.isEmpty())
 			return ResponseEntity.notFound().build();
 
-		EventResource eventResource = new EventResource(optionalEvent.get());
+		Event event = optionalEvent.get();
+		EventResource eventResource = new EventResource(event);
 		eventResource
 				.add(Link.of("/docs/index.html#resources-events-get","profile"));
+		if (event.getManager().equals(currentUser)) {
+			eventResource.add(linkTo(EventController.class).slash(event.getId()).withRel("update-event"));
+		}
 		return ResponseEntity.ok(eventResource);
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity updateEvent(@PathVariable Integer id,
-			@RequestBody @Valid EventDto eventDto, Errors errors) {
+	public ResponseEntity updateEvent(@PathVariable Integer id, @RequestBody @Valid EventDto eventDto, Errors errors,
+			@CurrentUser Account currentUser) {
 
 		Optional<Event> optionalEvent = this.eventRepository.findById(id);
 		if (optionalEvent.isEmpty()) {
@@ -101,6 +118,11 @@ public class EventController {
 		this.eventValidator.validate(eventDto, errors);
 		if (errors.hasErrors()) {
 			return getErrorsResourceResponseEntity(errors);
+		}
+
+		Event existingevent = optionalEvent.get();
+		if (!existingevent.getManager().equals(currentUser)) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
 		}
 
 		Event existingEvent = optionalEvent.get();
